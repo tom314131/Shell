ad3b9de023897067e0b0651c248d04a167ef35c0
tom <tom314131@gmail.com>
2019-03-28 00:45:20 +0200

    Update Project

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/tom/CLionProjects/ex1
+# Install script for directory: /home/tom/CLionProjects/Shell
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/tom/CLionProjects/ex1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/tom/CLionProjects/Shell/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
